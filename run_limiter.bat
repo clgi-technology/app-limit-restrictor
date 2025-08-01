05d1db532c6a4b46065f648c235deb53a07b330b
clgi-technology <ron.wright@clgi.org>
2025-07-31 22:23:31 -0500

    Update run_limiter.bat

--- run_limiter.bat
+++ run_limiter.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Adjust python path if needed
-SET PYTHON_PATH=C:\Users\melba\AppData\Local\Programs\Python\Python313\python.exe
-SET SCRIPT_PATH=C:\Users\melba\Downloads\activitywatch.py
+SET PYTHON_PATH=C:\Users\user\AppData\Local\Programs\Python\Python313\python.exe
+SET SCRIPT_PATH=C:\Users\user\Downloads\activitywatch.py
 
 REM Run the python script
 "%PYTHON_PATH%" "%SCRIPT_PATH%"
